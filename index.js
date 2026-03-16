a4b74ac350c0bbf3db4831a3ffbd17f41a89af71
Pra <155552124+bistpramod@users.noreply.github.com>
2026-03-16 23:17:24 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+//! typescript
+
+//* data types 
+//number
+//string
+//oolean
+// numm 
+// undefined 
+//enum
+
+//any
+//unknown
+
+
+//variable declaration methods
+// let const var
+
+//? number
+let b: number =34;
+b= 23;
+//b = true
+
+
+//?string
+let c : string = "";
+
+
