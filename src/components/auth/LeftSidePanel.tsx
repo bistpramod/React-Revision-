b8866fb536745dd9b837093e220b86203e953207
bistpramod <bistpramod113@gmail.com>
2026-03-17 23:52:28 +0545

    Initial React Vite project setup

---
+++ src/components/auth/LeftSidePanel.tsx
@@ -0,0 +1,17 @@
+
+
+export default function LeftSidePanel() {
+  return (
+    <div className="w-1/3 bg-emerald-800 rounded-md p-10 items-center justify-center flex flex-col gap-5 text-white">
+    
+
+      
+      <p className="text-center p-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+        doloremque accusantium facilis nostrum amet? Eveniet eius quae beatae
+        voluptates odit ullam nostrum temporibus animi iusto laborum.
+        Consectetur libero repudiandae quibusdam!
+      </p>
+    </div>
+  );  
+}
